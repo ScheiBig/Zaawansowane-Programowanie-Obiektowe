d7f623feb9a96dbdb3ba66c1ae356ac16d0fed00
Marcin Jeznach <Scheissig@gmail.com>
2024-06-07 12:59:14 +0200

    add MainCalc @ lab 3
    Entry point for calculator application.

---
+++ Lab_3/src/main/java/lab_3/MainCalc.java
@@ -0,0 +1,118 @@
+package lab_3;
+
+import lab_3.number.NumberOps;
+import lab_3.util.Args;
+import lab_3.util.Res;
+import org.jetbrains.annotations.Nullable;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Optional;
+
+public class MainCalc {
+	private static String currentArg;
+	private static double firstOp;
+	private static double secondOp;
+	private static String operation;
+
+	public static void mainCalc(@Nullable String[] args)
+	throws
+			InvocationTargetException,
+			IllegalAccessException,
+			IllegalArgumentException,
+			UnsupportedOperationException {
+
+		Iterator<String> argz;
+		if (args == null) {
+			argz = Args.stdin();
+			System.out.println("Enter equation:");
+		} else {
+			argz = Args.of(args);
+		}
+
+		var numOps = NumberOps.class.getDeclaredMethods();
+		Method op;
+
+		if (!argz.hasNext()) {
+			throw new IllegalArgumentException("You need to provide arguments for calculator!");
+		}
+		currentArg = argz.next();
+
+		var firstArg = Res.from(() -> Double.parseDouble(currentArg));
+		if (firstArg.err() == null) {
+			firstOp = firstArg.val();
+		} else {
+			operation = currentArg;
+		}
+
+		if (!argz.hasNext()) {
+			throw new IllegalArgumentException(
+					"You need to provide at least arguments for calculator!");
+		}
+		currentArg = argz.next();
+
+		if (operation != null) {
+			op = Arrays.stream(numOps)
+					.filter(m -> m.getName()
+							.equals(operation))
+					.findFirst()
+					.orElseThrow(() -> new UnsupportedOperationException("Single-argument " +
+							"operation " +
+							operation +
+							" is not implemented"));
+
+			var secondArg = Res.from(() -> Double.parseDouble(currentArg));
+			if (secondArg.err() == null) {
+				firstOp = secondArg.val();
+			} else {
+				throw new IllegalArgumentException("Argument " +
+						currentArg +
+						" is not a number - required for single-argument operation " +
+						operation);
+			}
+
+			System.out.print(operation + "( " + firstOp + " ) = " + op.invoke(null, firstOp));
+			return;
+		}
+
+		operation = currentArg;
+
+		op = Arrays.stream(numOps)
+				.filter(m -> m.getName()
+						.equals(operation))
+				.findFirst()
+				.orElseThrow(() -> new UnsupportedOperationException("Double-argument operation " +
+						operation +
+						" is not implemented"));
+
+
+		if (!argz.hasNext()) {
+			throw new IllegalArgumentException(
+					"You need to provide at three arguments for calculator with operation " +
+							operation +
+							"!");
+		}
+
+		currentArg = argz.next();
+
+		var secondArg = Res.from(() -> Double.parseDouble(currentArg));
+		if (secondArg.err() == null) {
+			secondOp = secondArg.val();
+		} else {
+			throw new IllegalArgumentException("Argument " +
+					currentArg +
+					" is not a number - required for double-argument operation " +
+					operation);
+		}
+
+		System.out.print(operation +
+				"( " +
+				firstOp +
+				", " +
+				secondOp +
+				" ) = " +
+				op.invoke(null, firstOp, secondOp));
+	}
+}
