4780262ceedbe9642909e486dc0e3fe24dbeb921
Marcin Jeznach <Scheissig@gmail.com>
2024-06-07 12:51:52 +0200

    add NumberOps @ lab 3

---
+++ Lab_3/src/main/java/lab_3/number/NumberOps.java
@@ -0,0 +1,13 @@
+package lab_3.number;
+
+public class NumberOps {
+	public static double plus(double a, double b) { return a + b; }
+
+	public static double minus(double a, double b) { return a - b; }
+
+	public static double times(double a, double b) { return a * b; }
+
+	public static double divide(double a, double b) { return a / b; }
+
+	public static double sqrt(double a) { return Math.sqrt(a); }
+}
