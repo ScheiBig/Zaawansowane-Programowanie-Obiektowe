8481ce2c5b79d62283604334e35149c6efbd2843
Marcin Jeznach <Scheissig@gmail.com>
2024-06-07 13:00:13 +0200

    add application branching in Main @ lab 3

--- Lab_3/src/main/java/lab_3/Main.java
+++ Lab_3/src/main/java/lab_3/Main.java
@@ -1,8 +1,37 @@
 package lab_3;
 
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.Scanner;
+
 public class Main {
 
 
 	public static void main(String[] args) {
+		try {
+			if (args.length >= 1) {
+				switch (args[0]) {
+					case "calc":
+						MainCalc.mainCalc(Arrays.copyOfRange(args, 1, args.length - 1));
+						return;
+					case "student":
+						return;
+				}
+			} else {
+				var sc = new Scanner(System.in);
+				System.out.println("Select program type ['calc' / 'student']:");
+				switch (sc.nextLine()) {
+					case "calc":
+						MainCalc.mainCalc(null);
+						return;
+					case "student":
+						return;
+				}
+			}
+		} catch (Throwable e) {
+			System.err.println(e.getMessage());
+			Optional.ofNullable(e.getCause())
+					.ifPresent(er -> System.err.println(er.getMessage()));
+		}
 	}
 }
