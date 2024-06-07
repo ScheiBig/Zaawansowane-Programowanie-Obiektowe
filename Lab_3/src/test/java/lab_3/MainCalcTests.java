8d3d00c5d27f06d2014117e9a77dc09f85327fbd
Marcin Jeznach <Scheissig@gmail.com>
2024-06-07 13:01:09 +0200

    scaffold tests fo MainCalc @ lab 3

---
+++ Lab_3/src/test/java/lab_3/MainCalcTests.java
@@ -0,0 +1,35 @@
+package lab_3;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MainCalcTests {
+
+	@Test void Test_no_args() {
+
+	}
+
+	@Test void Test_only_one_arg() {
+
+	}
+
+	@Test void Test_unknown_single_arg_operation() {
+
+	}
+
+	@Test void Test_bad_number_for_single_arg_operation() {
+
+	}
+
+	@Test void Test_unknown_double_arg_operation() {
+
+	}
+
+	@Test void Test_bad_second_number_for_double_arg_operation() {
+
+	}
+
+	@Test void Test_proper_calculations() {
+
+	}
+}
