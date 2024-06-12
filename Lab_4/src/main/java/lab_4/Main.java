384c66267d5f740359990054999b08d5d17b07dc
Marcin Jeznach <scheissig@gmail.com>
2024-06-12 20:40:01 +0200

    add DSL for JavaFX, prettify layout of GUI @ lab 4

--- Lab_4/src/main/java/lab_4/Main.java
+++ Lab_4/src/main/java/lab_4/Main.java
@@ -1,5 +1,27 @@
 package lab_4;
 
+import javafx.application.Application;
+import javafx.beans.property.Property;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.Event;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
+import lab_4.concurrent.Semaphore;
+import lab_4.concurrent.locks.TwoWayMonitor;
+import lab_4.io.BufferingMode;
+import lab_4.io.ReaderThread;
+import lab_4.io.WriterThread;
+import lab_4.javafx.utils.FX;
+
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -11,24 +33,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.event.Event;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.TextAlignment;
-import javafx.stage.Stage;
-import lab_4.concurrent.Semaphore;
-import lab_4.concurrent.locks.TwoWayMonitor;
-import lab_4.io.BufferingMode;
-import lab_4.io.ReaderThread;
-import lab_4.io.WriterThread;
-
 public class Main
 		extends Application
 {
@@ -39,7 +43,7 @@ public class Main
 	static {
 		try {
 			pliki = new ArrayList<>();
-			Arrays.stream(new File("src/main/resources/in2").listFiles())
+			Arrays.stream(new File("src/main/resources/in").listFiles())
 					.map(File::toURI)
 					.map(uri -> {
 						try {
@@ -67,34 +71,49 @@ public class Main
 	@Override
 	public void start(Stage stage)
 	throws URISyntaxException, IOException, InterruptedException {
-		var vbox = new VBox();
-		var hbox = new HBox();
-		var label = new Label();
-		var scene = new Scene(vbox, 360, 240);
-		var options =
-				FXCollections.observableArrayList(BufferingMode.values());
-		var comboBox = new ComboBox<>(options);
-		var button = new Button("Uruchom");
+		var button = new Button[1];
+		var comboBox = new ComboBox[1];
 
+		stage.setScene(FX.scene(
+				FX.vBox((vb, vb_ch) -> {
+
+					vb_ch.add(FX.region(r -> VBox.setVgrow(r, Priority.ALWAYS)));
+					vb_ch.add(FX.label(l -> {
+						l.setText("""
+								Okno główne aplikacji
+								Zamknięcie tego okna spowoduje bezpieczne zamknięcie aplikacji
+								Wybierz tryb programu, i uruchom:""");
+						l.setAlignment(Pos.CENTER);
+						l.setTextAlignment(TextAlignment.CENTER);
+						l.setMaxWidth(Double.MAX_VALUE);
+					}));
+					vb_ch.add(FX.region(r -> r.setPrefHeight(10.0)));
+					vb_ch.add(FX.hBox((hb, hb_ch) -> {
+						hb_ch.add(FX.region(r -> HBox.setHgrow(r, Priority.ALWAYS)));
+						hb_ch.add(FX.comboBox(cb -> {
+							cb.setItems(FXCollections.observableArrayList(BufferingMode.values()));
+							cb.setValue(BufferingMode.ByChar);
+							comboBox[0] = cb;
+						}));
+						hb_ch.add(FX.region(r -> r.setPrefWidth(20.0)));
+						hb_ch.add(FX.button(b -> {
+							b.setText("Uruchom");
+							button[0] = b;
+						}));
+						hb_ch.add(FX.region(r -> HBox.setHgrow(r, Priority.ALWAYS)));
+					}));
+					vb_ch.add(FX.region(r -> VBox.setVgrow(r, Priority.ALWAYS)));
+
+				}),
+				FX.NodeInitializer::noInit
+		));
 		stage.setTitle("Zadanie 1");
-		stage.setScene(scene);
 		stage.show();
 		stage.setX(20);
 		stage.setY(20);
+		stage.setWidth(380);
+		stage.setHeight(240);
 		stage.setAlwaysOnTop(true);
-
-		label.setText("Okno główne aplikacji\n" + "Zamknięcie tego okna spowoduje bezpieczne " +
-				"zamknięcie aplikacji\n" + "Wybierz tryb programu, i uruchom:");
-		label.setAlignment(Pos.CENTER);
-		label.setTextAlignment(TextAlignment.CENTER);
-
-		vbox.getChildren()
-				.addAll(label, hbox);
-		hbox.getChildren()
-				.addAll(comboBox, button);
-
-		comboBox.setValue(BufferingMode.ByChar);
-
 
 		var waitGroup = new Semaphore(pliki.size(), true);
 		var monitor = new TwoWayMonitor();
@@ -133,7 +152,12 @@ public class Main
 				);
 				readers.add(rt);
 				rt.window.show();
-				rt.window.setX(20 + stage.getWidth() + 20 + out.window.getWidth() + 20 + (rt.window.getWidth() + 20) * i);
+				rt.window.setX(20 +
+						stage.getWidth() +
+						20 +
+						out.window.getWidth() +
+						20 +
+						(rt.window.getWidth() + 20) * i);
 				rt.window.setY(20);
 				rt.window.setAlwaysOnTop(true);
 
@@ -148,12 +172,13 @@ public class Main
 			System.exit(0);
 		});
 
-		button.setOnMouseClicked(e -> {
-			readers.forEach(r -> r.setBufferingMode(comboBox.getValue()));
+		button[0].setOnMouseClicked(e -> {
+			readers.forEach(r -> r.setBufferingMode((BufferingMode) comboBox[0].getValue()));
 			readers.forEach(Thread::start);
 			out.start();
-			button.setOnMouseClicked(Event::consume);
-			button.setDisable(true);
+			button[0].setOnMouseClicked(Event::consume);
+			button[0].setDisable(true);
+			comboBox[0].setDisable(true);
 		});
 
 		System.out.println(this.getParameters()
---
+++ Lab_4/src/main/java/lab_4/javafx/utils/FX.java
@@ -0,0 +1,358 @@
+package lab_4.javafx.utils;
+
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
+public final class FX {
+
+	// High order not-nodes
+
+	public static Stage stage(NodeInitializer<Stage> init) {
+		return initAndReturn(new Stage(), init);
+	}
+
+	public static Scene scene(
+			Parent root,
+			NodeInitializer<Scene> init
+	) {
+		return initAndReturn(new Scene(root), init);
+	}
+
+
+	// Layout nodes
+
+	public static AnchorPane anchorPane(LayoutPaneInitializer<AnchorPane> init) {
+		return initWithChildrenAndReturn(new AnchorPane(), init);
+	}
+
+	public static BorderPane borderPane(LayoutPaneInitializer<BorderPane> init) {
+		return initWithChildrenAndReturn(new BorderPane(), init);
+	}
+
+	public static FlowPane flowPane(LayoutPaneInitializer<FlowPane> init) {
+		return initWithChildrenAndReturn(new FlowPane(), init);
+	}
+
+	public static GridPane gridPane(LayoutPaneInitializer<GridPane> init) {
+		return initWithChildrenAndReturn(new GridPane(), init);
+	}
+
+	public static HBox hBox(LayoutPaneInitializer<HBox> init) {
+		return initWithChildrenAndReturn(new HBox(), init);
+	}
+
+	public static StackPane stackPane(LayoutPaneInitializer<StackPane> init) {
+		return initWithChildrenAndReturn(new StackPane(), init);
+	}
+
+	public static TilePane tilePane(LayoutPaneInitializer<TilePane> init) {
+		return initWithChildrenAndReturn(new TilePane(), init);
+	}
+
+	public static VBox vBox(LayoutPaneInitializer<VBox> init) {
+		return initWithChildrenAndReturn(new VBox(), init);
+	}
+
+	public static Region region(NodeInitializer<Region> init) {
+		return initAndReturn(new Region(), init);
+	}
+
+
+	// Control nodes
+
+	public static Accordion accordion(NodeInitializer<Accordion> init) {
+		return initAndReturn(new Accordion(), init);
+	}
+
+	public static Alert alert(
+			Alert.AlertType type,
+			NodeInitializer<Alert> init
+	) {
+		return initAndReturn(new Alert(type), init);
+	}
+
+	public static Button button(NodeInitializer<Button> init) {
+		return initAndReturn(new Button(), init);
+	}
+
+	public static ButtonBar buttonBar(NodeInitializer<ButtonBar> init) {
+		return initAndReturn(new ButtonBar(), init);
+	}
+
+	public static <T> Cell<T> cell(NodeInitializer<Cell<T>> init) {
+		return initAndReturn(new Cell<>(), init);
+	}
+
+	public static CheckBox checkBox(NodeInitializer<CheckBox> init) {
+		return initAndReturn(new CheckBox(), init);
+	}
+
+	public static <T> CheckBoxTreeItem<T> checkBoxTreeItem(NodeInitializer<CheckBoxTreeItem<T>> init) {
+		return initAndReturn(new CheckBoxTreeItem<>(), init);
+	}
+
+	public static CheckMenuItem checkMenuItem(NodeInitializer<CheckMenuItem> init) {
+		return initAndReturn(new CheckMenuItem(), init);
+	}
+
+	public static <T> ChoiceBox<T> choiceBox(NodeInitializer<ChoiceBox<T>> init) {
+		return initAndReturn(new ChoiceBox<>(), init);
+	}
+
+	public static <T> ChoiceDialog<T> choiceDialog(NodeInitializer<ChoiceDialog<T>> init) {
+		return initAndReturn(new ChoiceDialog<>(), init);
+	}
+
+	public static ColorPicker colorPicker(NodeInitializer<ColorPicker> init) {
+		return initAndReturn(new ColorPicker(), init);
+	}
+
+	public static <T> ComboBox<T> comboBox(NodeInitializer<ComboBox<T>> init) {
+		return initAndReturn(new ComboBox<>(), init);
+	}
+
+	public static ContextMenu contextMenu(NodeInitializer<ContextMenu> init) {
+		return initAndReturn(new ContextMenu(), init);
+	}
+
+	public static CustomMenuItem customMenuItem(NodeInitializer<CustomMenuItem> init) {
+		return initAndReturn(new CustomMenuItem(), init);
+	}
+
+	public static DateCell dateCell(NodeInitializer<DateCell> init) {
+		return initAndReturn(new DateCell(), init);
+	}
+
+	public static DatePicker datePicker(NodeInitializer<DatePicker> init) {
+		return initAndReturn(new DatePicker(), init);
+	}
+
+	public static <R> Dialog<R> dialog(NodeInitializer<Dialog<R>> init) {
+		return initAndReturn(new Dialog<>(), init);
+	}
+
+	public static DialogPane dialogPane(NodeInitializer<DialogPane> init) {
+		return initAndReturn(new DialogPane(), init);
+	}
+
+	public static Hyperlink hyperlink(NodeInitializer<Hyperlink> init) {
+		return initAndReturn(new Hyperlink(), init);
+	}
+
+	public static Label label(NodeInitializer<Label> init) {
+		return initAndReturn(new Label(), init);
+	}
+
+	public static <T> ListCell<T> listCell(NodeInitializer<ListCell<T>> init) {
+		return initAndReturn(new ListCell<>(), init);
+	}
+
+	public static <T> ListView<T> listView(NodeInitializer<ListView<T>> init) {
+		return initAndReturn(new ListView<>(), init);
+	}
+
+	public static Menu menu(NodeInitializer<Menu> init) {
+		return initAndReturn(new Menu(), init);
+	}
+
+	public static MenuBar menuBar(NodeInitializer<MenuBar> init) {
+		return initAndReturn(new MenuBar(), init);
+	}
+
+	public static MenuButton menuButton(NodeInitializer<MenuButton> init) {
+		return initAndReturn(new MenuButton(), init);
+	}
+
+	public static MenuItem menuItem(NodeInitializer<MenuItem> init) {
+		return initAndReturn(new MenuItem(), init);
+	}
+
+	public static Pagination pagination(NodeInitializer<Pagination> init) {
+		return initAndReturn(new Pagination(), init);
+	}
+
+	public static PasswordField passwordField(NodeInitializer<PasswordField> init) {
+		return initAndReturn(new PasswordField(), init);
+	}
+
+	public static PopupControl popupControl(NodeInitializer<PopupControl> init) {
+		return initAndReturn(new PopupControl(), init);
+	}
+
+	public static ProgressBar progressBar(NodeInitializer<ProgressBar> init) {
+		return initAndReturn(new ProgressBar(), init);
+	}
+
+	public static ProgressIndicator progressIndicator(NodeInitializer<ProgressIndicator> init) {
+		return initAndReturn(new ProgressIndicator(), init);
+	}
+
+	public static RadioButton radioButton(NodeInitializer<RadioButton> init) {
+		return initAndReturn(new RadioButton(), init);
+	}
+
+	public static RadioMenuItem radioMenuItem(NodeInitializer<RadioMenuItem> init) {
+		return initAndReturn(new RadioMenuItem(), init);
+	}
+
+	public static ScrollBar scrollBar(NodeInitializer<ScrollBar> init) {
+		return initAndReturn(new ScrollBar(), init);
+	}
+
+	public static ScrollPane scrollPane(NodeInitializer<ScrollPane> init) {
+		return initAndReturn(new ScrollPane(), init);
+	}
+
+	public static Separator separator(NodeInitializer<Separator> init) {
+		return initAndReturn(new Separator(), init);
+	}
+
+	public static SeparatorMenuItem separatorMenuItem(NodeInitializer<SeparatorMenuItem> init) {
+		return initAndReturn(new SeparatorMenuItem(), init);
+	}
+
+	public static Slider slider(NodeInitializer<Slider> init) {
+		return initAndReturn(new Slider(), init);
+	}
+
+	public static <T> Spinner<T> spinner(NodeInitializer<Spinner<T>> init) {
+		return initAndReturn(new Spinner<>(), init);
+	}
+
+	public static SplitMenuButton splitMenuButton(NodeInitializer<SplitMenuButton> init) {
+		return initAndReturn(new SplitMenuButton(), init);
+	}
+
+	public static SplitPane splitPane(NodeInitializer<SplitPane> init) {
+		return initAndReturn(new SplitPane(), init);
+	}
+
+	public static SplitPane.Divider splitPaneDivider(NodeInitializer<SplitPane.Divider> init) {
+		return initAndReturn(new SplitPane.Divider(), init);
+	}
+
+	public static Tab tab(NodeInitializer<Tab> init) {
+		return initAndReturn(new Tab(), init);
+	}
+
+	public static <S, T> TableCell<S, T> tableCell(NodeInitializer<TableCell<S, T>> init) {
+		return initAndReturn(new TableCell<S, T>(), init);
+	}
+
+	public static <S, T> TableColumn<S, T> tableColumn(NodeInitializer<TableColumn<S, T>> init) {
+		return initAndReturn(new TableColumn<>(), init);
+	}
+
+	public static <T> TableRow<T> tableRow(NodeInitializer<TableRow<T>> init) {
+		return initAndReturn(new TableRow<>(), init);
+	}
+
+	public static <S> TableView<S> tableView(NodeInitializer<TableView<S>> init) {
+		return initAndReturn(new TableView<>(), init);
+	}
+
+	public static TabPane tabPane(NodeInitializer<TabPane> init) {
+		return initAndReturn(new TabPane(), init);
+	}
+
+	public static TextArea textArea(NodeInitializer<TextArea> init) {
+		return initAndReturn(new TextArea(), init);
+	}
+
+	public static TextField textField(NodeInitializer<TextField> init) {
+		return initAndReturn(new TextField(), init);
+	}
+
+	public static TextInputDialog textInputDialog(NodeInitializer<TextInputDialog> init) {
+		return initAndReturn(new TextInputDialog(), init);
+	}
+
+	public static TitledPane titledPane(NodeInitializer<TitledPane> init) {
+		return initAndReturn(new TitledPane(), init);
+	}
+
+	public static ToggleButton toggleButton(NodeInitializer<ToggleButton> init) {
+		return initAndReturn(new ToggleButton(), init);
+	}
+
+	public static ToggleGroup toggleGroup(NodeInitializer<ToggleGroup> init) {
+		return initAndReturn(new ToggleGroup(), init);
+	}
+
+	public static ToolBar toolBar(NodeInitializer<ToolBar> init) {
+		return initAndReturn(new ToolBar(), init);
+	}
+
+	public static Tooltip tooltip(NodeInitializer<Tooltip> init) {
+		return initAndReturn(new Tooltip(), init);
+	}
+
+	public static <T> TreeCell<T> treeCell(NodeInitializer<TreeCell<T>> init) {
+		return initAndReturn(new TreeCell<>(), init);
+	}
+
+	public static <T> TreeItem<T> treeItem(NodeInitializer<TreeItem<T>> init) {
+		return initAndReturn(new TreeItem<>(), init);
+	}
+
+	public static <S, T> TreeTableCell<S, T> treeTableCell(NodeInitializer<TreeTableCell<S, T>> init) {
+		return initAndReturn(new TreeTableCell<>(), init);
+	}
+
+	public static <S, T> TreeTableColumn<S, T> treeTableColumn(NodeInitializer<TreeTableColumn<S,
+			T>> init) {
+		return initAndReturn(new TreeTableColumn<>(), init);
+	}
+
+	public static <T> TreeTableRow<T> treeTableRow(NodeInitializer<TreeTableRow<T>> init) {
+		return initAndReturn(new TreeTableRow<>(), init);
+	}
+
+	public static <S> TreeTableView<S> treeTableView(NodeInitializer<TreeTableView<S>> init) {
+		return initAndReturn(new TreeTableView<>(), init);
+	}
+
+	public static <T> TreeView<T> treeView(NodeInitializer<TreeView<T>> init) {
+		return initAndReturn(new TreeView<>(), init);
+	}
+
+
+	// Privates
+
+	private static <N> N initAndReturn(
+			N node,
+			NodeInitializer<N> init
+	) {
+		init.init(node);
+		return node;
+	}
+
+	private static <P extends Pane> P initWithChildrenAndReturn(
+			P node,
+			LayoutPaneInitializer<P> init
+	) {
+		init.init(node, node.getChildren());
+		return node;
+	}
+
+	@FunctionalInterface
+	public interface NodeInitializer<N> {
+
+		void init(N node);
+
+		static <N> void noInit(N n) {}
+	}
+
+	@FunctionalInterface
+	public interface LayoutPaneInitializer<P extends Pane> {
+
+		void init(P node, ObservableList<Node> children);
+
+		static <P extends Pane> void noInit(P p, ObservableList<Node> c) {}
+	}
+}
