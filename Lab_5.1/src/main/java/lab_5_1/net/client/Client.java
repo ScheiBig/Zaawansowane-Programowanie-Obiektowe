34763e89d9eca9cc3d4322c0388f0a04717d91a3
Marcin Jeznach <scheissig@gmail.com>
2024-06-17 23:55:56 +0200

    add Client @ lab 5.1
    Client provided as Runnable, because default
    behavior is to block main() on client - but allows
    for launching it as daemon.

---
+++ Lab_5.1/src/main/java/lab_5_1/net/client/Client.java
@@ -0,0 +1,48 @@
+package lab_5_1.net.client;
+
+import utilx.concurrent.locks.Monitor;
+import utilx.net.SocketIO;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class Client
+		implements Runnable
+{
+
+	private final String host;
+	private final int port;
+	private final Monitor outLock;
+
+	public Client(
+			String host,
+			int port
+	) {
+		this.host = host;
+		this.port = port;
+		this.outLock = new Monitor();
+	}
+
+	@Override
+	public void run() {
+		try {
+			var socket = new Socket(this.host, this.port);
+			System.out.println("~CLIENT~: Connected to server @ " + this.host + ":" + this.port);
+			System.out.println("""
+					\tType `LIST_USERS` to get list of other char users.
+					\tType `REGISTER` <username> to register.
+					\tType `HELP` to see available commands.""");
+			var socketIO = new SocketIO(socket).objectStreams();
+			var write = new Client_WriteThread(socketIO, this.outLock);
+			write.start();
+			var read = new Client_ReadThread(socketIO, write::initUsername, this.outLock);
+			read.start();
+
+			write.join();
+			read.join();
+		} catch (IOException | InterruptedException e) {
+			throw new RuntimeException(e);
+		}
+
+	}
+}
