c38185f9645b7ce146b5b5824b3102cfdcd89bb0
Marcin Jeznach <scheissig@gmail.com>
2024-06-17 23:36:33 +0200

    refactor client WriteThread @ lab 5.1
    Removed AtomicReference wrapping from username,
    as ReadThread will not need username.
    
    Changed username initialization method.
    
    Removed try-with-resources - on client, ReadThread
    will manage socket connection.
    
    Wrapped calls to System.err / System.out with
    monitor, to prevent splits in the middle
    of messages.

--- Lab_5.1/src/main/java/lab_5_1/net/client/Client_WriteThread.java
+++ Lab_5.1/src/main/java/lab_5_1/net/client/Client_WriteThread.java
@@ -2,33 +2,41 @@ package lab_5_1.net.client;
 
 import lab_5_1.net.Msg;
 import org.jetbrains.annotations.Nullable;
+import utilx.concurrent.locks.Monitor;
 import utilx.net.SocketIO;
 
 import java.io.IOException;
 import java.util.Scanner;
-import java.util.concurrent.atomic.AtomicReference;
 
 public class Client_WriteThread
 		extends Thread
 {
 
 	private final SocketIO.ObjectStreamIO socketIO;
-	private final AtomicReference<@Nullable String> username = new AtomicReference<>();
+	private final Monitor outLock;
+	private @Nullable String username;
+	private @Nullable String pendingUsername;
 
-	public Client_WriteThread(SocketIO.ObjectStreamIO socketIO) {
+	public Client_WriteThread(
+			SocketIO.ObjectStreamIO socketIO,
+			Monitor outLock
+	) {
 		this.socketIO = socketIO;
+		this.outLock = outLock;
 
-		this.username.set(null);
+		this.username = null;
 	}
 
-	public void initUsername(String username) {
-		this.username.set(username);
+	public void initUsername() {
+		this.username = this.pendingUsername;
 	}
 
 	@Override
 	public void run() {
+		// Client_ReadThread is responsible for cleanup
+		//noinspection resource
+		var push = socketIO.out();
 		try (
-				var push = socketIO.out();
 				var scanner = new Scanner(System.in);
 		) {
 			String[] input;
@@ -42,62 +50,100 @@ public class Client_WriteThread
 
 				switch (input[0]) {
 					case Msg.Register.Cmd -> {
-						if (this.username.get() != null) {
-							System.err.println("Already registered");
+						if (this.username != null) {
+							this.outLock.lock();
+							try {
+								System.err.println("Already registered");
+							} finally {
+								this.outLock.unlock();
+							}
 							continue;
 						}
 						if (input.length != 2) {
-							System.err.println("""
-									Username required.
-									Type `HELP REGISTER` for details""");
+							this.outLock.lock();
+							try {
+								System.err.println("""
+										Username required.
+										Type `HELP REGISTER` for details""");
+							} finally {
+								this.outLock.unlock();
+							}
 							continue;
 						}
-						push.writeObject(new Msg.Register(input[1]));
+						this.pendingUsername = input[1];
+						push.writeObject(new Msg.Register(this.pendingUsername));
 					}
 					case Msg.ListUsers.Cmd -> {
 						push.writeObject(new Msg.ListUsers());
 					}
 					case Msg.All.Cmd -> {
-						if (this.username.get() == null) {
-							System.err.println("You need to register first");
+						if (this.username == null) {
+							this.outLock.lock();
+							try {
+								System.err.println("You need to register first");
+							} finally {
+								this.outLock.unlock();
+							}
 							continue;
 						}
 						if (input.length != 2) {
-							System.err.println("""
-									Message required.
-									Type `HELP ALL` for details.""");
+							this.outLock.lock();
+							try {
+								System.err.println("""
+										Message required.
+										Type `HELP ALL` for details.""");
+							} finally {
+								this.outLock.unlock();
+							}
 							continue;
 						}
 						push.writeObject(new Msg.All(input[1]));
 					}
 					case Msg.Exit.Cmd -> {
-						push.writeObject(new Msg.Exit(this.username.get()));
+						push.writeObject(new Msg.Exit(this.username));
 						break loop;
 					}
 					case Msg.Help.Cmd -> {
-						if (input.length == 1) {
-							System.out.println(Msg.Help.help());
-						} else {
-							switch (input[1]) {
-								case Msg.Register.Cmd -> System.out.println(Msg.Register.help());
-								case Msg.ListUsers.Cmd -> System.out.println(Msg.ListUsers.help());
-								case Msg.All.Cmd -> System.out.println(Msg.All.help());
-								case Msg.SendTo.Cmd -> System.out.println(Msg.SendTo.help());
-								case Msg.Exit.Cmd -> System.out.println(Msg.Exit.help());
-								case Msg.Help.Cmd -> System.out.println(Msg.Help.help());
-								default -> System.err.println(Msg.UnknownCommand);
+						this.outLock.lock();
+						try {
+							if (input.length == 1) {
+								System.out.println(Msg.Help.help());
+							} else {
+								switch (input[1]) {
+									case Msg.Register.Cmd ->
+											System.out.println(Msg.Register.help());
+									case Msg.ListUsers.Cmd ->
+											System.out.println(Msg.ListUsers.help());
+									case Msg.All.Cmd -> System.out.println(Msg.All.help());
+									case Msg.SendTo.Cmd -> System.out.println(Msg.SendTo.help());
+									case Msg.Exit.Cmd -> System.out.println(Msg.Exit.help());
+									case Msg.Help.Cmd -> System.out.println(Msg.Help.help());
+									default -> System.err.println(Msg.UnknownCommand);
+								}
 							}
+						} finally {
+							this.outLock.unlock();
 						}
 					}
 					default -> {
-						if (this.username.get() == null) {
-							System.err.println("You need to register first");
+						if (this.username == null) {
+							this.outLock.lock();
+							try {
+								System.err.println("You need to register first");
+							} finally {
+								this.outLock.unlock();
+							}
 							continue;
 						}
 						if (input.length != 2) {
-							System.err.println("""
-									Message required.
-									Type `HELP SEND_TO` for details.""");
+							this.outLock.lock();
+							try {
+								System.err.println("""
+										Message required.
+										Type `HELP SEND_TO` for details.""");
+							} finally {
+								this.outLock.unlock();
+							}
 							continue;
 						}
 						push.writeObject(new Msg.SendTo(input[0], input[1]));
