0e94df9d8642dde8c6aa15159d195528bfdc832f
Marcin Jeznach <scheissig@gmail.com>
2024-06-21 22:36:15 +0200

    add missing changes in client @ lab 5.2

--- Lab_5.2/src/main/java/lab_5_2/ClientApplication.java
+++ Lab_5.2/src/main/java/lab_5_2/ClientApplication.java
@@ -1,11 +1,9 @@
 package lab_5_2;
 
 import javafx.application.Application;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
 import lab_5_2.client.ClientController;
 
 public class ClientApplication
