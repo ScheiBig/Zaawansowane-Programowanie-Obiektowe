7b6cbeaef5b8f869937c95ef9823adbf2bcaa677
Marcin Jeznach <scheissig@gmail.com>
2024-06-21 22:21:19 +0200

    add deps for result type @ lab 5.2

---
+++ Lab_5.2/src/main/java/utilx/result/Result.java
@@ -0,0 +1,29 @@
+package utilx.result;
+
+import java.util.Optional;
+
+/**
+ * Wraps generic result of calculation, that either produces value, or error.
+ * <p>
+ * By contract, it is expected that:
+ * <p><code>
+ * (this.val() == null) != (this.err() == null)
+ * </code>
+ * so that, R{@link Result} exclusively wraps either value, or error. For Legal, empty values,
+ * {@link Optional}s should be used.
+ *
+ * @param <V> Type of wrapped value
+ * @param <E> Type of wrapped error
+ */
+public interface Result<V, E extends Throwable> {
+
+	/**
+	 * Returns value.
+	 */
+	V val();
+
+	/**
+	 * Returns error.
+	 */
+	E err();
+}
